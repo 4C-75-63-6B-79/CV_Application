e7083ed78d3cb0811001e0d2625e5f469cfbb438
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-09 10:19:34 +0530

    created experience js which renders a fieldset with legend experience

---
+++ src/components/experience.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Experience extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <fieldset>
+                <legend>{"Experience"}</legend>
+            </fieldset>
+        )
+    }
+}
+
+export default Experience;
