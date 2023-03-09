476283920bdd2af761e938e432cbe4539f5d0fc8
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-09 10:18:06 +0530

    created education js which renders a fieldset with legend education

---
+++ src/components/education.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Education extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <fieldset>
+                <legend>{"Education"}</legend>
+            </fieldset>
+        )
+    }
+}
+
+export default Education;
