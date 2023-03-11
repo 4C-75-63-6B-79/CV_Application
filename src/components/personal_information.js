132e49a745a8364ffb088a9a6ec8603e69c5cc55
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-11 11:17:40 +0530

    created the function to change in input value for the each input field and sent them as props to the input fields like onChangeMethod

--- src/components/personal_information.js
+++ src/components/personal_information.js
@@ -12,7 +12,8 @@ class PersonalInformation extends React.Component {
                 placeholder: 'First Name',
                 minlength: '1',
                 maxlength: '25',
-                pattern: '[A-Z]{1}[A-Za-z]{1,24}'
+                pattern: '[A-Z]{1}[A-Za-z]{1,24}',
+                onChangeMethod: this.inputFirstNameValueChanged.bind(this)
             },
             {
                 type: 'text',
@@ -20,7 +21,8 @@ class PersonalInformation extends React.Component {
                 placeholder: 'Last Name',
                 minlength: '3',
                 maxlength: '25',
-                pattern: '[A-Z]{1}[A-Za-z]{3,24}'
+                pattern: '[A-Z]{1}[A-Za-z]{3,24}',
+                onChangeMethod: this.inputLastNameValueChanged.bind(this)
             },
             {
                 type: 'text',
@@ -28,7 +30,8 @@ class PersonalInformation extends React.Component {
                 placeholder: 'Title',
                 minlength: '2',
                 maxlength: '3',
-                pattern: 'Mr|Mrs|Ms'
+                pattern: 'Mr|Mrs|Ms',
+                onChangeMethod: this.inputTitleValueChanged.bind(this)
             },
             {
                 type: "text",
@@ -36,7 +39,8 @@ class PersonalInformation extends React.Component {
                 placeholder: 'Address',
                 minlength: '4',
                 maxlength: '50',
-                pattern: '[-/,A-Za-z0-9 ]{4,50}'
+                pattern: '[-/,A-Za-z0-9 ]{4,50}',
+                onChangeMethod: this.inputAddressValueChanged.bind(this)
             },
             {
                 type: 'text',
@@ -44,7 +48,8 @@ class PersonalInformation extends React.Component {
                 placeholder: 'Phone Number',
                 minlength: "10",
                 maxlength: '10',
-                pattern: '[1-9]{1}[0-9]{9,9}'
+                pattern: '[1-9]{1}[0-9]{9,9}',
+                onChangeMethod: this.inputPhoneNumberValueChanged.bind(this)
             },
             {
                 type: "email",
@@ -52,7 +57,8 @@ class PersonalInformation extends React.Component {
                 placeholder: 'Email',
                 minlength: '10',
                 maxlength: "10",
-                pattern: "[0-9a-zA-Z_]{1,}@{1}[A-Za-z]{1,}[/.]{1}com"
+                pattern: "[0-9a-zA-Z_]{1,}@{1}[A-Za-z]{1,}[/.]{1}com",
+                onChangeMethod: this.inputEmailValueChanged.bind(this)
             }
         ]
         this.state = {
@@ -63,12 +69,35 @@ class PersonalInformation extends React.Component {
         }
     }
 
+    inputFirstNameValueChanged(event) {
+        console.log(this);
+    }
+
+    inputLastNameValueChanged(event) {
+        console.log(this);
+    }
+
+    inputTitleValueChanged(event) {
+        console.log(this);
+    }
+
+    inputAddressValueChanged(event) {
+        console.log(this);
+    }
+
+    inputPhoneNumberValueChanged(event) {
+        console.log(this);
+    }
+
+    inputEmailValueChanged(event) {
+        console.log(this);
+    }
 
     render() {
         const inputElements = this.htmlInputProperties.map((inputElementProp) => {
-            const {type, name, placeholder, minlength, maxlength, pattern} = inputElementProp;
+            const {type, name, placeholder, minlength, maxlength, pattern, onChangeMethod} = inputElementProp;
             const {value, errorMessage} = this.state;
-            return <Input key={name} type={type} name={name} placeholder={placeholder} value={value} pattern={pattern} minlength={minlength} maxlength={maxlength} errorMessage={errorMessage}/>
+            return <Input key={name} type={type} name={name} placeholder={placeholder} value={value} pattern={pattern} minlength={minlength} maxlength={maxlength} errorMessage={errorMessage} onChangeMethod={onChangeMethod}/>
         });
         return(
             <fieldset>
