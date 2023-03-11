40bf1aef7516c53c3ccbcd2f0b8df311e1fa726e
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-11 11:16:01 +0530

    added a functiont o call onchangemethod which is props

--- src/components/input.js
+++ src/components/input.js
@@ -1,15 +1,29 @@
 import React from "react";
+import Error from "./error_message";
 
 class Input extends React.Component {
     
     constructor(props) {
         super(props);
+        this.inputValueChanged = this.inputValueChanged.bind(this);
+        this.state = {
+            errorMessage: this.props.errorMessage,
+            className: 'error'
+        }
+    }
+
+    inputValueChanged(event) {
+        console.log(event.target.value);
+        this.props.onChangeMethod(event);
     }
 
     render() {
-        const {type, name, placeholder, pattern, minLength, maxLength} = this.props;
+        const {type, name, placeholder, value, pattern, minLength, maxLength} = this.props;
         return(
-            <input type={type} name={name} placeholder={placeholder} pattern={pattern} minLength={minLength} maxLength={maxLength} required></input>
+            <div>
+                <input type={type} name={name} placeholder={placeholder} value={value} pattern={pattern} minLength={minLength} maxLength={maxLength} onChange={this.inputValueChanged} required></input>
+                <Error className={this.state.className} errorMessage={this.state.errorMessage}/>
+            </div>
         );
     }
 }
