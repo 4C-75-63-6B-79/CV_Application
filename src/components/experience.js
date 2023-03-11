4250de9d4c5ebaf4cf846b3a15cc4b57e0446712
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-11 18:30:40 +0530

    created the basic onChangemethod for all the input fields which update the states and added the input validation to the position and also modified the pattern of the to and from input fields

--- src/components/experience.js
+++ src/components/experience.js
@@ -13,7 +13,7 @@ class Experience extends React.Component {
                 minlength: "3",
                 maxlength: "20",
                 pattern: "[A-Z]{1}[A-Za-z ]{2,20}",
-                onChangeMethod: this.inputValueChange.bind(this),
+                onChangeMethod: this.inputPositionValueChanged.bind(this),
             },
             {
                 type: "text",
@@ -22,7 +22,7 @@ class Experience extends React.Component {
                 minlength: "3",
                 maxlength: "20",
                 pattern: "[A-Z]{1}[A-Za-z ]{2,20}",
-                onChangeMethod: this.inputValueChange.bind(this)
+                onChangeMethod: this.inputCompanyValueChanged.bind(this)
             },
             {
                 type: "text",
@@ -31,7 +31,7 @@ class Experience extends React.Component {
                 minlength: "3",
                 maxlength: "20",
                 pattern: "[A-Z]{1}[A-Za-z ]{2,20}",
-                onChangeMethod: this.inputValueChange.bind(this)
+                onChangeMethod: this.inputCityValueChanged.bind(this)
             },
             {
                 type: "text",
@@ -39,8 +39,8 @@ class Experience extends React.Component {
                 minlength: "9",
                 maxlength: "15",
                 placeholder: "May, 2021",
-                pattern:"[a-z]{9}, [0-9]{4}",
-                onChangeMethod: this.inputValueChange.bind(this)
+                pattern:"((January)|(February)|(March)|(April)|(May)|(June)|(July)|(August)|(September)|(October)|(November)|(December)), ((19)|(20))[0-9]{2}",
+                onChangeMethod: this.inputFromValueChanged.bind(this)
             },
             {
                 type: "text",
@@ -48,8 +48,8 @@ class Experience extends React.Component {
                 minlength: "9",
                 maxlength: "15",
                 placeholder: "March, 2022",
-                pattern:"[a-z]{9}, [0-9]{4}",
-                onChangeMethod: this.inputValueChange.bind(this)
+                pattern:"((January)|(February)|(March)|(April)|(May)|(June)|(July)|(August)|(September)|(October)|(November)|(December)), ((19)|(20))[0-9]{2}",
+                onChangeMethod: this.inputToValueChanged.bind(this)
             }
         ];
         this.state = {
@@ -76,8 +76,76 @@ class Experience extends React.Component {
         }
     }
 
-    inputValueChange(event) {
-        console.log(this);
+    inputPositionValueChanged(event) {
+        const inputValue = event.target.value;
+        const attributeValue = event.target.name;
+        const isValid = event.target.validity.valid;
+        let errorMessage = ""
+        if(isValid) {
+            errorMessage = "Input is valid.";
+        } else {
+            if(!(/^[A-Z]/.test(inputValue.charAt(0)))) {
+                errorMessage = 'First letter of position should be capital.';
+            } else if(inputValue.length < 3) {
+                errorMessage = "Position should be atleast 3 characters long.";
+            } else if(inputValue.length > 20) {
+                errorMessage = "Postion should be atmost 20 characters long.";
+            }
+        }
+        this.setState({
+            "Position": {
+                value: inputValue,
+                errorMessage: errorMessage
+            }
+        });
+    }
+
+    inputCompanyValueChanged(event) {
+        console.log(event.target.validity.valid);
+        const inputValue = event.target.value;
+        const attributeValue = event.target.name;
+        this.setState({
+            "Company": {
+                value: inputValue,
+                errorMessage: ""
+            }
+        });
+    }
+
+    inputCityValueChanged(event) {
+        console.log(event.target.validity.valid);
+        const inputValue = event.target.value;
+        const attributeValue = event.target.name;
+        this.setState({
+            "City": {
+                value: inputValue,
+                errorMessage: ""
+            }
+        });
+    }
+
+    inputFromValueChanged(event) {
+        console.log(event.target.validity.valid);
+        const inputValue = event.target.value;
+        const attributeValue = event.target.name;
+        this.setState({
+            "From": {
+                value: inputValue,
+                errorMessage: ""
+            }
+        });
+    }
+
+    inputToValueChanged(event) {
+        console.log(event.target.validity.valid);
+        const inputValue = event.target.value;
+        const attributeValue = event.target.name;
+        this.setState({
+            "To": {
+                value: inputValue,
+                errorMessage: ""
+            }
+        });
     }
 
     render() {
