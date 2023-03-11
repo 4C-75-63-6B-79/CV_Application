414a6a7bc7fff8c4762bd2eef31e10cfa67625b9
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-11 10:18:20 +0530

    created a submit button for form and made a submitButtonclicked method

--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,25 @@
 import React from "react";
 import PersonalInformation from "./personal_information";
+import Button from "./button";
 
 class Form extends React.Component {
 
     constructor(props) {
         super(props);
+        this.submitButtonClicked = this.submitButtonClicked.bind(this);
+    }
+
+    submitButtonClicked() {
+        console.log('submit button clicked');
     }
 
     render() {
+        const fromSubmitButton = <Button textContent="Submit" onClickMethod={this.submitButtonClicked}/>;
+
         return(
             <form method={"#"}>
                 <PersonalInformation />
+                {fromSubmitButton}
             </form>
         );
     }
