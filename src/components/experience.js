57d47ffd8f83ae916d0a5af0db79ae4573740f4f
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-11 18:02:25 +0530

    created a state and htminputproperties in constructor and also create the input element using the componenent input and then rendered them

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,15 +1,95 @@
 import React from "react";
+import Input from "./input";
 
 class Experience extends React.Component {
 
     constructor(props) {
         super(props);
+        this.htmlInputProperties = [
+            {
+                type: "text",
+                name: "Position",
+                placeholder: "Position",
+                minlength: "3",
+                maxlength: "20",
+                pattern: "[A-Z]{1}[A-Za-z ]{2,20}",
+                onChangeMethod: this.inputValueChange.bind(this),
+            },
+            {
+                type: "text",
+                name: "Company",
+                placeholder: "Company",
+                minlength: "3",
+                maxlength: "20",
+                pattern: "[A-Z]{1}[A-Za-z ]{2,20}",
+                onChangeMethod: this.inputValueChange.bind(this)
+            },
+            {
+                type: "text",
+                name: "City",
+                placeholder: "Position",
+                minlength: "3",
+                maxlength: "20",
+                pattern: "[A-Z]{1}[A-Za-z ]{2,20}",
+                onChangeMethod: this.inputValueChange.bind(this)
+            },
+            {
+                type: "text",
+                name: "From",
+                minlength: "9",
+                maxlength: "15",
+                placeholder: "May, 2021",
+                pattern:"[a-z]{9}, [0-9]{4}",
+                onChangeMethod: this.inputValueChange.bind(this)
+            },
+            {
+                type: "text",
+                name: "To",
+                minlength: "9",
+                maxlength: "15",
+                placeholder: "March, 2022",
+                pattern:"[a-z]{9}, [0-9]{4}",
+                onChangeMethod: this.inputValueChange.bind(this)
+            }
+        ];
+        this.state = {
+            "Position": {
+                value: "",
+                errorMessage: ""
+            },
+            "Company": {
+                value: "",
+                errorMessage: ""
+            },
+            "City": {
+                value: "",
+                errorMessage: ""
+            },
+            "From": {
+                value: "",
+                errorMessage: ""
+            },
+            "To": {
+                value: "",
+                errorMessage: ""
+            },
+        }
+    }
+
+    inputValueChange(event) {
+        console.log(this);
     }
 
     render() {
+        const inputElements = this.htmlInputProperties.map((inputElementProp) => {
+            const {type, name, placeholder, minlength, maxlength, pattern, onChangeMethod} = inputElementProp;
+            const {value, errorMessage} = this.state[name];
+            return <Input key={name} type={type} name={name} placeholder={placeholder} value={value} pattern={pattern} minlength={minlength} maxlength={maxlength} errorMessage={errorMessage} onChangeMethod={onChangeMethod}/>
+        });
         return(
             <fieldset>
                 <legend>{"Experience"}</legend>
+                {inputElements}
             </fieldset>
         )
     }
