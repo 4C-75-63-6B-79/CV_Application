7e1a44923bbab75af7b44749aca1b53cd8662d26
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-12 20:09:30 +0530

    created the component cv to hold the cv thing

---
+++ src/components/cv.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class CV extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CV;
