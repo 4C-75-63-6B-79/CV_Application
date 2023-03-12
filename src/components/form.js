bfaeb95123a97e0388fb02a1636e19c67b3ab522
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-12 15:58:53 +0530

    created function related to the education information

--- src/components/form.js
+++ src/components/form.js
@@ -11,16 +11,24 @@ class Form extends React.Component {
            personalInformation: {},
            experienceIds: [0],
            experienceInformation: {},
-           numberOfEducation: 1,
-           educationInformation: []
+           educationIds: [0],
+           educationInformation: {}
         }
+        this.setPersonalInformation = this.setPersonalInformation.bind(this);
+
         this.addOneMoreExperienceButtonClicked = this.addOneMoreExperienceButtonClicked.bind(this);
         this.removeExperienceButtonClicked = this.removeExperienceButtonClicked.bind(this);
-        this.submitButtonClicked = this.submitButtonClicked.bind(this);
-        this.setPersonalInformation = this.setPersonalInformation.bind(this);
         this.setExperienceInformation = this.setExperienceInformation.bind(this);
         this.updateExperienceIds = this.updateExperienceIds.bind(this);
         this.updateExperienceInformation = this.updateExperienceInformation.bind(this);
+
+        this.addOneMoreEducationButtonClicked = this.addOneMoreEducationButtonClicked.bind(this);
+        this.removeEducationButtonClicked = this.removeEducationButtonClicked.bind(this);
+        this.setEducationInformation = this.setEducationInformation.bind(this);
+        this.updateEducationIds = this.updateEducationIds.bind(this);
+        this.updateEducationInformation = this.updateEducationInformation.bind(this);
+
+        this.submitButtonClicked = this.submitButtonClicked.bind(this);
     }
 
     setPersonalInformation(attributeName, attributeValue) {
@@ -74,6 +82,49 @@ class Form extends React.Component {
         this.updateExperienceInformation(experienceId);
     }
 
+    setEducationInformation(experienceId, attributeName, attributeValue) {
+        const experienceInformation = this.state.experienceInformation;
+        const experienceAtIndex = experienceInformation[experienceId] || {};
+        experienceAtIndex[attributeName] = attributeValue;
+        experienceInformation[experienceId] = experienceAtIndex;
+        this.setState({
+            experienceInformation: experienceInformation,
+        });
+    }
+
+    addOneMoreEducationButtonClicked(event) {
+        event.preventDefault();
+        const experienceIdPresent = this.state.experienceIds;
+        const lastExperienceId = experienceIdPresent.length === 0 ? -1 : experienceIdPresent[experienceIdPresent.length-1];
+        const newExperienceId = lastExperienceId + 1;
+        experienceIdPresent.push(newExperienceId);
+        this.setState({
+            experienceIds: experienceIdPresent
+        });
+    }
+
+    updateEducationInformation(experienceId) {
+        const updatedExperienceInformation = this.state.experienceInformation;
+        delete updatedExperienceInformation[experienceId];
+        this.setState({
+            experienceInformation: updatedExperienceInformation
+        });
+    }
+
+    updateEducationIds(experienceId) {
+        const updatedExperienceIds = this.state.experienceIds;
+        const indexOfExperienceToBeRemoved = updatedExperienceIds.indexOf(experienceId);
+        updatedExperienceIds.splice(indexOfExperienceToBeRemoved, 1);
+        this.setState({
+            experienceIds: updatedExperienceIds
+        });
+    }
+
+    removeEducationButtonClicked(experienceId) {
+        this.updateExperienceIds(experienceId);
+        this.updateExperienceInformation(experienceId);
+    }
+
     submitButtonClicked(event) {
         event.preventDefault();
         console.log(this.state);
@@ -93,6 +144,11 @@ class Form extends React.Component {
                     <legend>{"Experience"}</legend>
                     {experienceElemets}
                     {addOneMoreExperienceButton}
+                </fieldset>
+                <fieldset>
+                    <legend>{"Education"}</legend>
+                    
+
                 </fieldset>
                 {fromSubmitButton}
             </form>
