2c1499f7fb9d2d820a3a639ed6b63b8d0bc5bfcb
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-13 10:53:19 +0530

    added the cv compoent and passed it the data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Form from "./components/form";
+import CV from "./components/cv";
 
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      information: {
+        personalInformation: {},
+        experienceInformation: {},
+        educationInformation: {},
+      }
+    }
+    this.informationUpdated = this.informationUpdated.bind(this);
+  }
+
+  informationUpdated(information) {
+    this.setState({
+      information: information
+    });
+  }
+
   render() {
     return(
       <div>
-        <Form />
+        <Form onInformationUpdate={this.informationUpdated}/>
+        <CV cvInformation={this.state.information}/>
       </div>
     );
   }
