0c437107b5b98dfdf5ed3723950d014e68119fb4
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-13 10:29:43 +0530

    made the header with data

--- src/components/cv_header.js
+++ src/components/cv_header.js
@@ -7,12 +7,16 @@ class CVHeader extends React.Component {
     }
 
     render() {
+        const firstName = this.props.information['First Name'] || "";
+        const lastName = this.props.information['Last Name'] || "";
+        const title = this.props.information['Title'] || "";
+        const phoneNumber = this.props.information['Phone Number'] || "";
+        const email = this.props.information['Email'] || "";
+        const address = this.props.information['Address'] || "";
         return(
             <header>
-                <h1>{"Name"}</h1>
-                <p>{"Phone Number"}</p>
-                <p>{"Email"}</p>
-                <p>{"Address"}</p>
+                <h1>{title + " " + firstName + " " + lastName}</h1>
+                <p>{"Ph: "+ phoneNumber+ ", email: " + email + ", address: " + address }</p>
             </header>
         )
     }
